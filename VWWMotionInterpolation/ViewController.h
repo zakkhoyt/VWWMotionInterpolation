33ffe36de71e09b1364be5196620d2149068547d
Zakk Hoyt <zakk@threefoldphotos.com>
2015-05-29 16:57:30 -0700

    Initial Commit

---
+++ VWWMotionInterpolation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VWWMotionInterpolation
+//
+//  Created by Zakk Hoyt on 5/29/15.
+//  Copyright (c) 2015 Zakk Hoyt. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
